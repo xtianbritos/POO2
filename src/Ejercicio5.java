f328fe7430c973aad2ca3694db556e7e0ed99e52
Xtian <xtiancuenta1@gmail.com>
2022-11-28 18:49:09 -0300

    Ejercicio 5 hecho

---
+++ src/Ejercicio5.java
@@ -0,0 +1,12 @@
+public class Ejercicio5 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 11; i++) {
+            for (int j = 1; j < 11; j++) {
+                System.out.print(i*j + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
